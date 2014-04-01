4e3fd3144c4b9cb88c94d272fc2a3bb65d9650dd
John Treacy <john@johntreacy.com>
2014-03-31 21:01:54 -0700

    more cleanup in demo code

--- demos/PcaWebSliders/lib/pca_server.js
+++ demos/PcaWebSliders/lib/pca_server.js
@@ -1,26 +1,31 @@
 // main library for handling pwm events
 // Events are pwm requests channel, value = offtime, on-time always 0 
 
-var socketio, io, PwmDriver, pwm, makePwm, Gpio, led, button;
+var socketio, io, PwmDriver, pwm, makePwm, Gpio;
 
 socketio = require('socket.io');
-//PwmDriver = require('adafruit-i2c-pwm-driver');
-//pwm = new PwmDriver(0x40);
 makePwm = require('adafruit-pca9685');
 pwm = makePwm({"freq": 50, "correctionFactor": 1.118});
+
+// deal with any signals and cleanup
 
 process.on('SIGINT', function(code) {
     console.log("\nCtrl-C caught ...");
     process.exit(0);
 });
+
 process.on('SIGHUP', function(code) {
     console.log("exiting ...");
     process.exit(0);
 });
+
 process.on('exit', function(code) {
     console.log("exiting ...");
     process.exit(0);
 });
+
+// Setup the socketio connection and listeners
+
 exports.listen = function(server) {
   io = socketio.listen(server);
   io.set('log level', 1);
@@ -40,6 +45,7 @@ function handlePwmRequest(socket) {
 	pwm.setPwm(ch, 0, v); 
       });
 }
+
 function handlePwmPulseRequest(socket) {
     socket.on('pwmpulse', function(channel, value) {
 	var ch = parseInt(channel);
@@ -47,11 +53,13 @@ function handlePwmPulseRequest(socket) {
 	pwm.setPulse(ch, v); 
       });
 }
+
 function handleStopRequest(socket) {
     socket.on('pwmstop', function() {
 	pwm.stop();
     });
 }
+
 function handleClientDisconnection(socket) {
   socket.on('disconnect', function() {
     pwm.stop();
